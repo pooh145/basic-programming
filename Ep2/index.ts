bbae00d26fc851e1e33f25ce0d7d2684ce9bee6d
Joon <jjunemee@gmail.com>
2024-07-23 12:09:05 +0700

    Update index.ts

--- Ep2/index.ts
+++ Ep2/index.ts
@@ -1,4 +1,4 @@
-function grade(homework:number,midterm:number,final:number)  {
+function grade(homework:number,midterm:number,final:number){
     
     let score:number=homework+midterm+final
 
@@ -6,13 +6,15 @@ if(score<50){
     return 'เกรดF'
 } else if(score<60){
     return 'เกรดD'
-}
-else if(score<70){
+} else if(score<70){
     return 'เกรดC'
+} else if(score<80){
+     return'เกรดB'
 }
- 
+  else (score>=80)
+     return'เกรดA'
 }
 
-console.log(grade(10,20,30))
-  
+
+console.log(grade(20,30,15))
 
