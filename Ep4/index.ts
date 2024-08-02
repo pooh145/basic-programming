74829edf5cbb28e1233359566d44e656138764c5
kraianan Rattana <wipu0811@gmail.com>
2024-08-02 11:50:44 +0700

    Ep4

---
+++ Ep4/index.ts
@@ -0,0 +1,76 @@
+function sayHi(dayOfWeek: number){
+    switch( (dayOfWeek)) {
+        case 1:
+           console.log('สวัสดีวันอาทิตย์')
+           break
+        case 2:
+           console.log('สวัสดีวันจันทร์')    
+           break
+        case 3:
+           console.log('สวัสดีวันอังคาร')
+           break
+        case 4:
+           console.log('สวัดีวันพุทธ')
+           break   
+        case 5:
+           console.log('สวัดีวันพฤหัสดี')
+           break   
+        case 6:
+           console.log('สวัดีวันศกุร์')
+           break   
+        case 7:
+           console.log('สวัดีวันเสาร์')
+           break   
+    }
+}
+
+sayHi(1)
+
+
+
+
+
+
+
+// const x: number = 41
+// const y: number = 46
+
+
+// const z: number =0.1 + 0.5
+// console.log(z)
+
+
+// const x: number = 41
+// const y: number = 46
+
+// console.log(y.toFixed(3))
+
+
+// const x: number =46
+
+// console.log(parseFloat('46.90'))
+
+
+// function fixedDecimal(n: number, digit:number){
+//     const x = n.toFixed(digit)
+//     return x
+// }
+
+// console.log(fixedDecimal(15.1888888,10))
+
+
+// function fixNumber(x:number){
+//  const  y = x.toLocaleString() 
+//  return y 
+// }
+ 
+// console.log (fixNumber(80000))
+
+// const str: string = "I can't do this"
+
+const str: string = " Wipu "
+
+console.log(str.indexOf('p'))
+
+
+
