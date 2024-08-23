32da688530b130c9071e32d641cb1bdb4f8be592
kraianan Rattana <wipu0811@gmail.com>
2024-08-23 11:27:21 +0700

    วนวูป

---
+++ Ep5/index.ts
@@ -0,0 +1,91 @@
+//let sum: number = 0  ทำแบบที่ 1
+
+//for (let i = 0; i <= 365; i++) {
+       // sum = sum + i
+
+//}
+//  console.log('total:', sum)
+
+
+
+//let i: number = 1
+  //let sum: number = 0
+
+  //while (i <=365){ // ทำได้สองแบบที่ 2
+   //sum = sum + i
+  //  i++
+ // }
+
+//console.log('total:', sum)
+
+
+
+//let i: number = 1
+///let sum: number = 0
+
+//do {
+ //    sum =sum + i
+   //  i++
+//}  while (i <=365)
+
+//console.log('total:', sum)
+
+
+
+
+//let i: number = 1
+ //let sum: number = 0
+
+ //for (let j = 1; j < 10; j++){
+      //i = i* 5
+ //}
+  //while (i <=365){
+    // sum = sum + i
+   // i++
+ // }
+  
+//console.log('total:', sum)
+
+
+
+//let sum : number = 0
+ //let isBeark: boolean = false
+ //let row: number = 0
+
+ //while (true) {
+     //console.log('วิพูลลวิลล่าโสด')
+     //row++
+     //if (row === 3 ) {
+        //   break
+
+      // }
+        
+   //  }
+
+
+   //let car1: string = 'Honda'
+   //let car2: string = 'Yamaha'
+   //let car3: string = 'Toyota'
+   //let car4: string = 'Suzuki'
+  
+  //const cars: string [] = ['Honda','Yamaha','Toyota','Suzuki']
+  //cars[1] ='Toyota'
+
+
+
+ // const cars: string [] = ['Honda','Yamaha','Toyota','Suzuki','Mazda']
+
+//for (let i = 0; i <cars.length; i++) {
+   //  if (cars[i]==='Yamaha') {
+    //     console.log('รถซื้อแกง จะแรงได้ไง')
+    // }
+//}
+
+
+
+//const cars: string[] = ['Honda','Yamaha','Toyota','Suzuki','Mazda']
+
+//cars. forEach(function(car:string){
+    // console.log(car)
+
+//})
