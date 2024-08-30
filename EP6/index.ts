18c368fe2c1159aa7da8f4e368a9983bdc06c65d
kraianan Rattana <wipu0811@gmail.com>
2024-08-30 11:11:24 +0700

    javaSC

---
+++ EP6/index.ts
@@ -0,0 +1,67 @@
+//const score: number[] = [10,23,26,30,33]
+
+//let sum: number = 0
+//for (let i = 0; i < score.length; i++) {
+   // sum = sum + score[i]
+//}
+//const avg = sum / score.length
+
+    //console.log('Total:',sum)
+    //console.log('Avg',avg)
+
+    //const studentsName: string = 'แนน,ภู,บีม,อันจี้,ลัดกี้,นัท,เอ็ม,ปลิ้ม,ก็อต'
+//const studentsNameArray = studentsName.split(',')
+
+//console.log(studentsNameArray.toString())
+
+
+
+//const  animls:string = 'แมว*หมา*นก*ปลา*'
+//const animlsArray: string[] = animls.split ('*')
+
+//console.log('เดิม:',animlsArray)
+
+//animlsArray.pop()//ดึงตัวสุดท้ายออก (ดึงหมาออก)
+
+//console.log('ดึงตัวท้าย',animlsArray)
+
+//animlsArray.push('กาบีบาร่า')
+
+//["หมา,แมว","กระต่าย","กาบีบาร่า"]
+//console.log('ใส่ใหม่:',animlsArray)
+
+//animlsArray.shift()
+
+//console.log('ดึงตัวหน้า:',animlsArray)
+
+//animlsArray.unshift('แรด')
+
+//console.log('ใส่ตัวหน้า',animlsArray)
+
+
+
+
+//const fruits = ["Banana","Orange","Apple","Mamgo"];{
+
+    //console.log('เรียง A-Z', fruits.sort)
+    //console.log('เรียง A-Z', fruits.concat)
+    //}
+
+
+
+
+    
+    
+    //const me = {
+     //   firstName: "Wipu",
+     //   lastName : 'Blookleorm',
+      //  age: 20
+  
+ // }
+  
+  //ประวัติของฉัน
+  
+  //ชื่อ: Wuttipong
+  //นามสกุล:Blookleorm
+  //อายุ: 20
+  
